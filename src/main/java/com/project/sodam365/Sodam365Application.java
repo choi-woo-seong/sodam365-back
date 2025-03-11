394015ae584c17348c0cc3a4dbda76d274943769
choi-woo-seong <goqus903@naver.com>
2025-03-11 10:27:12 +0900

    first commit

---
+++ src/main/java/com/project/sodam365/Sodam365Application.java
@@ -0,0 +1,13 @@
+package com.project.sodam365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sodam365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sodam365Application.class, args);
+	}
+
+}
